9530c655e17306a7cdab00970769429c290a0ba6
Queen-Nethu-MDV <liyomd24@gmail.com>
2025-05-31 00:39:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,10 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "FnlA3YTJ#jv2fXahhvAN4IWAuImPqLs6XnRQc1sgEiBjuZZeYjF8",
+  OWNER_NUM: process.env.OWNER_NUM || "94779702531",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/B2gnQ2rm/IMG-20250216-WA0140.jpg",
   ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 𝑳_𝑰_𝒀_𝑶🥶",
+  MODE: process. env.MODE || "Public",
 };
