433103ab8f7f0f6232bbcaef4a3d274885fe157c
Queen-Nethu-MDV <liyomd24@gmail.com>
2025-05-30 23:31:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,5 +10,5 @@ module.exports = {
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/B2gnQ2rm/IMG-20250216-WA0140.jpg",
-  
+  ALIVE_MSG: process.env.ALIVE_MSG || "Hello , I am alive now!!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 𝑳_𝑰_𝒀_𝑶🥶",
 };
