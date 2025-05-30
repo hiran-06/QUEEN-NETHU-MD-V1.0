0f91997c12c5f523d5c965111465ac89c69b37f0
Queen-Nethu-MDV <liyomd24@gmail.com>
2025-05-30 23:28:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,6 @@ module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
   PREFIX: process.env.PREFIX || ".",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/B2gnQ2rm/IMG-20250216-WA0140.jpg",
+  
 };
