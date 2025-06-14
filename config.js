90a9cf5b39dbfc6175c1864b4ed94e61e9dfb69b
Queen-Nethu-MDV <liyomd24@gmail.com>
2025-06-14 08:59:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "Sc4FmTrb#jdrq-Wj2QBtg6mhW6tVpUd8Q1-fCqplhMO23M3kjA_Y",
   MONGODB: process.env.MONGODB || "mongodb://mongo:TcRCWZGNTzIKxnsUQvteZksjPiglffwf@switchyard.proxy.rlwy.net:46726",
   OWNER_NUM: process.env.OWNER_NUM || "94779702531",
 };
